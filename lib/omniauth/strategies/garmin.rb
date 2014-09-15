e865a6c011ade60df5983b01bf16a9cb977cc6ab
kirillivanov <kivanov@hill30.com>
2014-09-15 17:10:47 +0400

    little fix

--- lib/omniauth/strategies/garmin.rb
+++ lib/omniauth/strategies/garmin.rb
@@ -1,10 +1,11 @@
-require 'omniauth-oauth'
+require 'omniauth'
+require 'omniauth/strategies/oauth'
 require 'multi_json'
 
 module OmniAuth
   module Strategies
     class Garmin < OmniAuth::Strategies::OAuth
-      option :name, 'concur'
+      option :name, 'garmin'
       option :client_options, {
 	    :site               => "https://connectapi.garmin.com",
 	    :request_token_path => "/oauth-service-1.0/oauth/request_token",
