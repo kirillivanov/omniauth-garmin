f4a97ff7461987138e753a3b7cc9bffae8ce0c2b
kirillivanov <kivanov@hill30.com>
2014-09-15 17:53:49 +0400

    set authorize_params

--- lib/omniauth/strategies/garmin.rb
+++ lib/omniauth/strategies/garmin.rb
@@ -4,14 +4,28 @@ require 'multi_json'
 
 module OmniAuth
   module Strategies
-    class Garmin < OmniAuth::Strategies::OAuth
-      option :name, 'garmin'
-      option :client_options, {
-	    :site               => "https://connectapi.garmin.com",
-	    :request_token_path => "/oauth-service-1.0/oauth/request_token",
-	    :access_token_path  => "/oauth-service-1.0/oauth/access_token",
-	    :authorize_url      => "http://connect.garmin.com/oauthConfirm"
-      }
-    end
+	class Garmin < OmniAuth::Strategies::OAuth
+		option :name, 'garmin'
+		
+		option :client_options, {
+			:site               => "https://connectapi.garmin.com",
+			:request_token_path => "/oauth-service-1.0/oauth/request_token",
+			:access_token_path  => "/oauth-service-1.0/oauth/access_token",
+			:authorize_url      => "http://connect.garmin.com/oauthConfirm"
+		}
+
+      	def request_phase
+        	puts authorize_params
+        	super
+      	end
+
+		def authorize_params
+	        super.tap do |params|
+	          if request.params['oauth_callback']
+	            params[:oauth_callback] = request.params['oauth_callback']
+	          end
+	        end
+      	end
+
   end
 end
